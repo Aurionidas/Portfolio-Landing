3adcc44c59368cc7b30dff3921395d143914caf4
Aurionidas <74695466+Aurionidas@users.noreply.github.com>
2024-11-08 01:18:45 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = innerWidth - 20
+canvas.height = innerHeight - 20
 
-let bola = new Bola(canvas.width / 2, canvas.height / 2)
-bola.dibujar()
+let bolas = []
+
+for (let i = 0; i < 33; i++)
+    bolas.push(new Bola(canvas.width/2, canvas.height/2))
+
+
+function animar(){
+
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+    bolas.forEach(bola => {
+        bola.dibujar()
+        bola.mover()
+    })
+
+    requestAnimationFrame(animar)
+}
+
+animar()
