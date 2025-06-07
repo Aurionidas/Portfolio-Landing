f6d7bc50e9571c088539d9535e9003d3eb7d2f7c
Aurionidas <74695466+Aurionidas@users.noreply.github.com>
2025-06-06 22:39:08 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,13 +2,27 @@ const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
 
 canvas.width = innerWidth - 20
-canvas.height = innerHeight - 50
+canvas.height = innerHeight - 20
+
 
 let bolas = []
 
 for (let i = 0; i < 60; i++)
     bolas.push(new Bola(canvas.width/2, canvas.height/2))
 
+function resizeCanvas() {
+    canvas.width = window.innerWidth - 20;
+    canvas.height = window.innerHeight - 20;
+    bolas.forEach(bola => {
+        bola.x = canvas.width / 2;
+        bola.y = canvas.height / 2;
+    });
+}
+
+// Rezising canvas
+resizeCanvas();
+
+window.addEventListener('resize', resizeCanvas);
 
 function animar(){
 
@@ -22,5 +36,7 @@ function animar(){
 
     requestAnimationFrame(animar)
 }
+
+
 
 animar()
